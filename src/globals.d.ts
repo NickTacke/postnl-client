eb96eaa7d7fd25dfd5ec0d0e12262265f33cd0b8
NickTacke <the.nick.tacke@gmail.com>
2026-06-02 12:45:37 +0200

    chore(core): pin @types/node and drop globals shim

--- src/globals.d.ts
+++
@@ -1,7 +0,0 @@
-// runtime web globals provided by bun/node18+ but missing from the pinned type packages
-declare function atob(data: string): string;
-declare function btoa(data: string): string;
-
-interface TextDecoder {
-  decode(input?: ArrayBufferView | ArrayBuffer): string;
-}
