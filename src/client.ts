d994fcefa3970869da8482ea1acdc2b1dc66e7dd
NickTacke <the.nick.tacke@gmail.com>
2026-06-02 13:16:03 +0200

    feat(client): wire barcode, shipping, return resources

--- src/client.ts
+++ src/client.ts
@@ -1,10 +1,19 @@
 import { type PostNLClientOptions, resolveConfig } from "./config";
 import { Transport } from "./core/http";
+import { BarcodeResource } from "./resources/barcode";
+import { ReturnResource } from "./resources/return";
+import { ShippingResource } from "./resources/shipping";
 
-// resources are attached in later slices
 export class PostNLClient {
-  protected readonly transport: Transport;
+  private readonly transport: Transport;
+  readonly barcode: BarcodeResource;
+  readonly shipping: ShippingResource;
+  readonly return: ReturnResource;
+
   constructor(options: PostNLClientOptions) {
     this.transport = new Transport(resolveConfig(options));
+    this.barcode = new BarcodeResource(this.transport);
+    this.shipping = new ShippingResource(this.transport);
+    this.return = new ReturnResource(this.transport);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,15 @@ export { decodeBase64, labelContentType, toDecodedLabel } from "./core/base64";
 export type { DecodedLabel } from "./core/base64";
 export * from "./constants/enums";
 export * from "./constants/product";
+
+export { BarcodeResource } from "./resources/barcode";
+export { ShippingResource } from "./resources/shipping";
+export { ReturnResource } from "./resources/return";
+export type { BarcodeV4Request, BarcodeV4Response } from "./resources/barcode/schema";
+export type {
+  ShipmentV4Request,
+  LabellingV4Request,
+  ConfirmV4Request,
+  ShipmentPostResponse,
+} from "./resources/shipping/schema";
+export type { ReturnGenerateRequest } from "./resources/return/schema";
